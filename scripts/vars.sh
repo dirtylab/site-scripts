e274dddf7642e97104451baf79f56f819e1303c9
sveinburne <jules.randolph@usherbrooke.ca>
2016-01-26 16:13:50 -0500

    fix path with dirname

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -2,28 +2,30 @@
 # lower-case definitions, not to risk any system-wide env conflict
 
 echo $source_dir
+
+projet_root_dir=$(dirname "$source_dir/..")
 # wiki
 wiki_repo_url="https://github.com/dirtylab/wiki"
-wiki_repo_dir=$(dirname "$source_dir/../wiki" )
+wiki_repo_dir="$projet_root_dir/wiki" 
 
 # jekyll
 
-jekyll_tmp_dir=$(dirname "$source_dir/../jekyll-build"  )
-jekyll_templates_dir=$(dirname "$source_dir/../jekyll"  )
+jekyll_tmp_dir= "$projet_root_dir/jekyll-build"
+jekyll_templates_dir= "$projet_root_dir/jekyll"
 jekyll_includes_dir="_includes"
 jekyll_build_dir="_site"
 
 # npm
-npm_dir=$(dirname "$source_dir/../client/"   )
+npm_dir= "$projet_root_dir/client/"
 
 # static site
 dest_repo_url="https://github.com/dirtylab/dirtylab.github.io"
-dest_repo_path=$(dirname "$source_dir/../dirtylab.github.io"   )
+dest_repo_path= "$projet_root_dir/dirtylab.github.io"
 
 # scripts
 fix_script_path="$source_dir/fixlinks.php"
 emojize_script_path="$source_dir/emojize/emojize.php"
 
 # meta
-meta_dir=$(dirname "$source_dir/../.build-meta"   )
+meta_dir= "$projet_root_dir/.build-meta"
 last_commit_file="$meta_dir/lastCommit"
