9a4d01d99a47852a4ed7a988df08fdc550887524
sveinburne <jules.randolph@usherbrooke.ca>
2016-01-26 15:42:01 -0500

    evaluation des chemins

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -3,25 +3,25 @@
 
 # wiki
 wiki_repo_url="https://github.com/dirtylab/wiki"
-wiki_repo_dir="$source_dir/../wiki"
+wiki_repo_dir=$(cd "$source_dir/../wiki" && pwd)
 
 # jekyll
-jekyll_tmp_dir="$source_dir/../jekyll-build"
-jekyll_templates_dir="$source_dir/../jekyll"
+jekyll_tmp_dir=$(cd "$source_dir/../jekyll-build" && pwd)
+jekyll_templates_dir=$(cd "$source_dir/../jekyll" && pwd)
 jekyll_includes_dir="_includes"
 jekyll_build_dir="_site"
 
 # npm
-npm_dir="$source_dir/../client/"
+npm_dir=$(cd "$source_dir/../client/"  && pwd)
 
 # static site
 dest_repo_url="https://github.com/dirtylab/dirtylab.github.io"
-dest_repo_path="$source_dir/../dirtylab.github.io"
+dest_repo_path=$(cd "$source_dir/../dirtylab.github.io"  && pwd)
 
 # scripts
 fix_script_path="$source_dir/fixlinks.php"
 emojize_script_path="$source_dir/emojize/emojize.php"
 
 # meta
-meta_dir="$source_dir/../.build-meta"
+meta_dir=$(cd "$source_dir/../.build-meta"  && pwd)
 last_commit_file="$meta_dir/lastCommit"
