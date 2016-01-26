9d764a78b2f27df3a013e8fe3219f50a7a6e4112
sveinburne <jules.randolph@usherbrooke.ca>
2016-01-26 15:51:56 -0500

    evaluation des chemins via dirname

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # lower-case definitions, not to risk any system-wide env conflict
 
+echo $source_dir
 # wiki
 wiki_repo_url="https://github.com/dirtylab/wiki"
-wiki_repo_dir=$(cd "$source_dir/../wiki" && pwd)
+wiki_repo_dir=$(dirname "$source_dir/../wiki" )
 
 # jekyll
-jekyll_tmp_dir=$(cd "$source_dir/../jekyll-build" && pwd)
-jekyll_templates_dir=$(cd "$source_dir/../jekyll" && pwd)
+
+jekyll_tmp_dir=$(dirname "$source_dir/../jekyll-build"  )
+jekyll_templates_dir=$(dirname "$source_dir/../jekyll"  )
 jekyll_includes_dir="_includes"
 jekyll_build_dir="_site"
 
 # npm
-npm_dir=$(cd "$source_dir/../client/"  && pwd)
+npm_dir=$(dirname "$source_dir/../client/"   )
 
 # static site
 dest_repo_url="https://github.com/dirtylab/dirtylab.github.io"
-dest_repo_path=$(cd "$source_dir/../dirtylab.github.io"  && pwd)
+dest_repo_path=$(dirname "$source_dir/../dirtylab.github.io"   )
 
 # scripts
 fix_script_path="$source_dir/fixlinks.php"
 emojize_script_path="$source_dir/emojize/emojize.php"
 
 # meta
-meta_dir=$(cd "$source_dir/../.build-meta"  && pwd)
+meta_dir=$(dirname "$source_dir/../.build-meta"   )
 last_commit_file="$meta_dir/lastCommit"
